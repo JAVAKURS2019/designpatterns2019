aeb38ec539f335bfbf6ebeb4089ee0a5f53dbcbc
Krzysztof Dziuban <krdziuban@pgs-soft.com>
2019-09-22 09:45:32 +0200

    First Excercise

---
+++ FirstExcercise/src/main/java/com/dcristoph/Person.java
@@ -0,0 +1,100 @@
+package com.dcristoph;
+
+import java.util.Date;
+
+public class Person {
+
+    private int number;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String street;
+    private int houseNumber;
+    private int telephoneNumber;
+    private Date birthDate;
+    private String sentence;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public int getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(int telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date brithDate) {
+        this.birthDate = brithDate;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public Person(int number, String firstName, String lastName, String city, String street, int houseNumber, int telephoneNumber, Date birthDate, String sentence) {
+        this.number = number;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.telephoneNumber = telephoneNumber;
+        this.birthDate = birthDate;
+        this.sentence = sentence;
+    }
+}
