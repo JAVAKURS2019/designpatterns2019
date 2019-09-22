1dfb74ddb3f96087673913b978ed9bc01632bae4
lukaszbrudniak <lukasz.brudniak@gmail.com>
2019-09-22 10:35:54 +0200

    cosmetic changes

--- src/main/java/singleton/MySingleton.java
+++ src/main/java/singleton/MySingleton.java
@@ -5,17 +5,20 @@ public class MySingleton {
 
     private static MySingleton instance;
 
-    private MySingleton(){}
-    static{
-        try{
+    private MySingleton() {
+    }
+
+    static {
+        try {
             instance = new MySingleton();
-        }catch(Exception e){
-            throw new RuntimeException("Exception occured in creating singleton instance");
+        } catch (Exception e) {
+            throw new RuntimeException("Exception!!! Sorry!");
         }
     }
 
-    public static MySingleton getInstance(){
+    public static MySingleton getInstance() {
         return instance;
+
     }
 }
 
